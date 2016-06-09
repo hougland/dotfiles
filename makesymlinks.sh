403ba2bdb8ef65b050f1b58777452a2378a19e98
Ricky Hougland <ricky.hougland@socrata.com>
2016-06-09 14:06:49 -0700

    rm git files bc symlinks isn't supported easily

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="aliases bash_profile bashrc gitconfig gitignore_global zshrc"
+files="aliases bash_profile bashrc zshrc"
 
 ##########
 
